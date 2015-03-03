60801cb4992fbd2830f49e218f653ce9d5092c3b
sdnorm <spencerdnorman@gmail.com>
2015-03-03 14:11:38 -0600

    syntax fix

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,9 +2,9 @@ class Api::CommentsController < ApplicationController
 
   def create
     render json: Shop.find(params[:id])
-    render json: Comment.new params.require(:comment).permit(:message)
-    render json:  @comment.shop
+    render json: Comment.new(params.require(:comment).permit(:message))
+    render json: @comment.shop
   end
-  
+
 
 end
