5e47c7072365faecbcac9d088fd8f00bd2635000
sdnorm <spencerdnorman@gmail.com>
2015-03-04 12:05:09 -0600

    trying to restart app on heroku

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -28,7 +28,6 @@ class Api::CommentsController < ApplicationController
 
   def create
     @shop = Shop.find(params[:id])
-
     @comment = Comment.new params.require(:comment).permit(:message)
     @comment.shop = @shop
     if @comment.save
